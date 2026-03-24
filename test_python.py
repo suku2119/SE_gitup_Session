372bd966408583e2106b4c5c39f61025dd5ef4a1
SK <sukumar@rvsls.com>
2026-03-24 13:28:26 +0530

    My second python program - via upload

---
+++ test_python.py
@@ -0,0 +1,9 @@
+import pyautogui
+#pyautogui.alert('Hello, world!')
+import time
+time.sleep(5)
+#x, y = pyautogui.position()
+#print(f'Current mouse position: ({x}, {y})')
+#pyautogui.moveTo(100, 100, duration=1)
+pyautogui.click(119, 132)
+#pyautogui.typewrite('Hello, world!')
